5515f884e28f7d87699aea39cc71a62c9d1637c9
Chethan R G <chethanrg37@gmail.com>
2025-06-14 12:57:51 +0530

    redme file

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/guruwatersupply/', // Make sure it starts and ends with a slash
+  base: '/guruwatersupply', // Make sure it starts and ends with a slash
 })
